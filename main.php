a5936c11c2fe8a36dd7e438f7a205df6232f2706
Steve A. Forget <VonJagdstrumel@users.noreply.github.com>
2015-04-10 18:31:12 +0200

    Added directory iteration over San Francisco region as main script.

--- main.php
+++ main.php
@@ -5,11 +5,20 @@ require_once 'vendor/autoload.php';
 use RegionCartographer\DbpfParser;
 use RegionCartographer\CityFactory;
 
-$parser = new DbpfParser(@$argv[1]);
-$factory = new CityFactory($parser);
 
-$cityMetadata = $factory->fetchCityMetadata();
-var_dump($cityMetadata);
+$directory = new DirectoryIterator(@$argv[1]);
 
-$cityTile = $factory->fetchCityTile($cityMetadata);
-var_dump($cityTile);
+foreach ($directory as $entry) {
+    if ($entry->isFile() && $entry->isReadable() && $entry->getExtension() == 'sc4') {
+        $parser = new DbpfParser($entry->getPathname());
+        $factory = new CityFactory($parser);
+
+        $cityMetadata = $factory->fetchCityMetadata();
+        var_dump($cityMetadata);
+
+        $cityTile = $factory->fetchCityTile($cityMetadata);
+        var_dump($cityTile);
+
+        imagepng($cityTile->getImageHandle(), 'examples/' . $cityMetadata->getGuid() . '.png');
+    }
+}
